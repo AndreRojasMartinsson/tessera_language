398519fa2becb09e88424863cefe35c08834048f
AndreRojasMartinsson <89520206+AndreRojasMartinsson@users.noreply.github.com>
2025-07-22 14:27:25 +0200

    created hir datastructure
    Signed-off-by: AndreRojasMartinsson <89520206+AndreRojasMartinsson@users.noreply.github.com>

--- src/compiler/hir/mod.rs
+++ src/compiler/hir/mod.rs
@@ -1,6 +1,54 @@
 use std::hash::Hash;
 
-mod top_level;
+use bumpalo::{Bump, collections::Vec};
+use gxhash::HashMap;
 
-#[derive(Debug, Default, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
-pub struct HirId(u128);
+use crate::hir::{
+    expr::HirExpr,
+    identifiers::{ExprId, ItemId, StmtId},
+    item::HirItem,
+    stmt::HirStmt,
+};
+
+mod expr;
+mod identifiers;
+mod item;
+mod stmt;
+mod top_level;
+mod ty;
+
+pub struct Hir<'hir> {
+    pub exprs: Vec<'hir, HirExpr<'hir>>,
+    pub stmts: Vec<'hir, HirStmt>,
+    pub items: Vec<'hir, HirItem<'hir>>,
+    arena: &'hir Bump,
+}
+
+impl<'hir> Hir<'hir> {
+    pub fn new(arena: &'hir Bump) -> Self {
+        Self {
+            exprs: Vec::new_in(&arena),
+            items: Vec::new_in(&arena),
+            stmts: Vec::new_in(&arena),
+            arena,
+        }
+    }
+
+    pub fn alloc_expr(&mut self, expr: HirExpr<'hir>) -> ExprId {
+        let idx = self.exprs.len();
+        self.exprs.push(expr);
+        ExprId(idx)
+    }
+
+    pub fn alloc_stmt(&mut self, stmt: HirStmt) -> StmtId {
+        let idx = self.stmts.len();
+        self.stmts.push(stmt);
+        StmtId(idx)
+    }
+
+    pub fn alloc_item(&mut self, item: HirItem<'hir>) -> ItemId {
+        let idx = self.items.len();
+        self.items.push(item);
+        ItemId(idx)
+    }
+}
